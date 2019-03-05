b4b359e6055ee0bc22bcb687c58c1ed56cd87b13
Zorg-Moris <mvr.leon@gmail.com>
2019-03-06 00:51:36 +0200

    delite key

--- js/functionRequest.js
+++ js/functionRequest.js
@@ -1,8 +1,7 @@
 "use strict";
 
 function request(coin) {
-    // let url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${coin}&tsyms=USD,EUR`;
-    let url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${coin}&tsyms=USD,EUR&api_key=INSERT-dfc2acaf9534414b9ceb2b44b77d6ec0743f4cf67d1313fd0a671596b12b47aa`;
+    let url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${coin}&tsyms=USD,EUR`;
     return new Promise(function (resolve, reject) {
         const xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
@@ -24,8 +23,7 @@ function request(coin) {
 
 function historicalRequest(coin, term) {
     let currency = state.chooseCurrensy;
-    // let url = `https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=${currency}&limit=${term}`;
-    let url = `https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=${currency}&limit=${term}&api_key=INSERT-dfc2acaf9534414b9ceb2b44b77d6ec0743f4cf67d1313fd0a671596b12b47aa`;
+    let url = `https://min-api.cryptocompare.com/data/histoday?fsym=${coin}&tsym=${currency}&limit=${term}`;
     console.log("currency", currency);
     return new Promise(function (resolve, reject) {
         const xhr = new XMLHttpRequest();
